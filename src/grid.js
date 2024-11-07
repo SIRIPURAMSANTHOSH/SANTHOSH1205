1a6c39bc053f6f43ff8dfba6c172bcfc9d5741a0
SIRIPURAMSANTHOSH <santhoshsiripuram2004@gmail.com>
2024-11-07 16:46:00 +0530

    Add files via upload

---
+++ src/grid.js
@@ -0,0 +1,16 @@
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+export default function BasicButtons() {
+  return (
+  <div>
+    <Stack direction="column" spacing={10}marginTop={5}marginLeft={5}width={100}>
+    <Button variant='outline'>santhosh</Button>
+    <Button variant='outline'>lokesh</Button>
+    <Button variant='outline'>omkar</Button>
+    </Stack>
+  </div>
+    
+
+);
+}
